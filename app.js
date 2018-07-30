b187977b561f7c0c775d571925408d8f6c132491
Daniel Potts <drp777@hotmail.com>
2018-07-30 18:20:02 -0500

    initiated npm created app.js and keys.js

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// to access the keys in keys.js
+var keys = require("./config.js");
+//this is an import statment//
+var Twitter = require('twitter');
+
+var twitter = new Twitter ({
+    consumer_key: 'pB7wuoGgNeVytiwOTcM5rjFoo',
+    consumer_secret: 'y5jGzDPc3tWRmirYvQFVgQLlHxFLsC8jJJFXb9baXzVotnNKfa',
+    access_token_key: '912111857481031680-r2o82iL1gLq3lL619BbhnWIhW8SnI4g',
+    access_token_secret: 'CB0DfVuIlvauU6fqX4sD9zl6Dkgs0xDhew6tdxWgz7jO8',
+});
+
+console.log("hello");
+
---
+++ config.js
@@ -0,0 +1,20 @@
+console.log(" this is loading properly");
+
+//authenticating the npm//
+var twitterKeys = {
+    consumer_key: 'pB7wuoGgNeVytiwOTcM5rjFoo',
+    consumer_secret: 'y5jGzDPc3tWRmirYvQFVgQLlHxFLsC8jJJFXb9baXzVotnNKfa',
+    access_token_key: '912111857481031680-r2o82iL1gLq3lL619BbhnWIhW8SnI4g',
+    access_token_secret: 'CB0DfVuIlvauU6fqX4sD9zl6Dkgs0xDhew6tdxWgz7jO8',
+  });
+
+  var spotifyKeys = {
+    id: "c92096f3f1a54eadbeb8ea5bf8412c98",
+    secret: "ee310377e6284707be654d92c60895db"
+};
+  //exports!
+module.exports = {
+    twitterKeys: twitterKeys,
+
+    
+}
